939aafafae7c7f989861663436cfd4696f9c2a66
Aswinchand321 <88029112+Aswinchand321@users.noreply.github.com>
2021-07-28 21:51:51 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = "@Movies_Club_2019",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
